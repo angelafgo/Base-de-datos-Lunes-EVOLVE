0eeb4255a908738c21aff2254c41fc80cce8dfed
angelafgo <A00829821@itesm.mx>
2021-11-03 12:02:07 -0500

    descargar librerías para el codigo

--- basededatos.py
+++ basededatos.py
@@ -1,1 +1,16 @@
-# Venta de automoviles marca Audi
+# SP - Venta de automoviles marca Audi
+# EVOLVE - 8 Octubre 2021
+# SAITD 2021-2022
+
+# Importar todas las librerías necesarias para el análisis
+import pandas as pd                # 
+import matplotlib.pyplot as plt    # librería que genera gráficas a partir de datos contenidos en listas.
+import seaborn as sns              # librería de visualización de datos para Python, crea atractivas gráficas.
+import numpy as np                 # librería especializada en cálculo numérico
+from scripy.stats import norm
+from sklearn.preprocessing import standarscaler
+from scipy import stats
+import warnings
+
+%matplotlib inline                
+ # ^ Nos permite ver las graficas al momento de pedirlas
